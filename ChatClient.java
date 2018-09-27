b5450cf1d528ef7774245734c47bf0a9039a25c0
Higgins, Isaac Michael <imh7@students.waikato.ac.nz>
2018-09-27 21:06:44 +1200

    Tried adding MulticastSocket and InetAddresses

--- ChatClient.java
+++ ChatClient.java
@@ -12,13 +12,27 @@ public class ChatClient {
 
 public static void main(String args[]){
 
-	MulticastSocket = new MulticastSocket;
+	InetAddress group = InetAddress.getByName(239.0.202.1);
+	MulticastSocket multicast = new MulticastSocket(40202);
+	byte[] buf = new byte[1500];
+	DatagramPacket dp = new DatagramPacket(buf, buf.length);
+
+
+
+
+	ms.receive(dp);
+	dp.getData(), dp.length();
+	DatagramPacket pkt = new DatagramPacket(buf, buf.length);
+
+	String address1 = "239.0.202.1";
+	InetAddress address = new address1.getHostAddress();
+	multicast.joinGroup(address);
 
 
 
 	}
-	
-	
-	
-	
+
+
+
+
 }
